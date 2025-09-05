3d594fdf7aae1c49651bba2972fd3662971ed3cc
kitiibwa harriet <harrietkarry2906@gmail.com>
2025-09-04 22:20:40 -0700

    initial changes

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// Get current year and set it in the footer
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Get last modified date and set it in the footer
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last modified: ${lastModified}`;
